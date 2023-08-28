908b0b7eb379e8ee95f7542e59e373a79e4c9cec
Yahir Moreno Barajas <100240827+BarajasY@users.noreply.github.com>
2023-08-27 23:46:23 -0600

    fixed array, now board is shown.

--- client/src/three/board.ts
+++ client/src/three/board.ts
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 
-const geometry = new THREE.PlaneGeometry(1, 1);
+const geometry = new THREE.PlaneGeometry(2, 2);
 
 export const whiteGeometry = new THREE.MeshBasicMaterial({color:"white", side: THREE.DoubleSide});
 export const blackGeometry = new THREE.MeshBasicMaterial({color:"black", side: THREE.DoubleSide});
@@ -11,9 +11,9 @@ export const MakeBoard = () => {
     const boardMeshArr:THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial>[] = [];
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
-            const mesh = new THREE.Mesh(geometry, (i+j%2) == 0 ? blackGeometry : whiteGeometry)
-            mesh.position.set(i, -2, j);
-            mesh.rotation.x = 80
+            const mesh = new THREE.Mesh(geometry, ((i+j)%2) == 0 ? blackGeometry : whiteGeometry)
+            mesh.position.set(j*2, -1, i*2);
+            mesh.rotation.x = 1.55
             boardMeshArr.push(mesh);
         }
     }
